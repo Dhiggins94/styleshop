d7d362b106fe0704e35ca46f19a9f6da1c2267a2
Divine Higgins <dhiggins94@gmail.com>
2023-04-25 17:14:59 -0400

    adding scss for navbar

--- client/styleshop/src/components/Navbar/Navbar.jsx
+++ client/styleshop/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,65 @@
 import React from "react";
+import"./Navbar.scss"
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import SearchIcon from "@mui/icons-material/Search";
 import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="wrapper">
-        <div className="left"></div>
-        <div className="item">
-          <img src="/img/flag.png" alt="" />
-          <ArrowDownwardIcon />
+        {/* left side of nav */}
+        <div className="left">
+          <div className="item">
+            <img src="/img/flag.png" alt="" />
+            <ArrowDownwardIcon />
+          </div>
+          <div className="item">
+            <span> USD</span>
+            <ArrowDownwardIcon />
+          </div>
+          <div className="item">
+            <Link to="/products/1">Women</Link>
+          </div>
+          <div className="item">
+            <Link to="/products/2">Men</Link>
+          </div>
+          <div className="item">
+            <Link to="/products/3">Children</Link>
+          </div>
         </div>
-
-        <div className="center"></div>
-        <div className="right"></div>
+        {/* center of navbar */}
+        <div className="center">
+          <Link to="/">STYLESHOP</Link>
+        </div>
+        {/* right side of navbar */}
+        <div className="right">
+          <div className="center">
+            <Link to="/">HomePage</Link>
+          </div>
+          <div className="center">
+            <Link to="/">About</Link>
+          </div>
+          <div className="center">
+            <Link to="/">Contact</Link>
+          </div>
+          <div className="center">
+            <Link to="/">Stores</Link>
+          </div>
+          <div className="icons">
+            <SearchIcon />
+            <PersonOutlineIcon />
+            <FavoriteBorderOutlinedIcon />
+            {/* SHOWS ICON ON THE CART */}
+            <div className="cartIcon">
+              <ShoppingCartOutlinedIcon />
+              <span>0</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );

aa752f8340045595016c647a9ecc3a97f2f1bb25
Divine Higgins <dhiggins94@gmail.com>
2023-04-25 17:02:15 -0400

    started navbar, added sass and mui icons

--- client/styleshop/src/components/Navbar/Navbar.jsx
+++ client/styleshop/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left"></div>
+        <div className="item">
+          <img src="/img/flag.png" alt="" />
+          <ArrowDownwardIcon />
+        </div>
 
-export default Navbar
+        <div className="center"></div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

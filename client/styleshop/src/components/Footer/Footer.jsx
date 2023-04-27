3708b98f23508338d4e307ee033cf893d0aa95bd
Divine Higgins <dhiggins94@gmail.com>
2023-04-27 13:44:29 -0400

    finished footer, starting homepage slider

--- client/styleshop/src/components/Footer/Footer.jsx
+++ client/styleshop/src/components/Footer/Footer.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
-
+import React from "react";
+import "./footer.scss"
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accesories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Sapien
+            et ligula ullamcorper malesuada proin libero nunc.
+          </span>
+        </div>
+        <div className="item">
+          <h1>Contact</h1>
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Sapien
+            et ligula ullamcorper malesuada proin libero nunc.
+          </span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span className="logo">StyleShop</span>
+          <span className="copyright">
+            Copyright© 2023. All rights reserved.
+          </span>
+        </div>
+        <div className="right">
+          <img src="/img/payments.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;

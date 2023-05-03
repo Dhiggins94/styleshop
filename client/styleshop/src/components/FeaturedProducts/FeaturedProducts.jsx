590c345aa6df13212d458d2a695ad9bc286f7822
Divine Higgins <dhiggins94@gmail.com>
2023-05-02 23:58:37 -0400

    starting featured products

---
+++ client/styleshop/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./FeaturedProducts.scss";
+
+
+const FeaturedProducts = () => {
+  return <div className="featuredProducts"></div>;
+};
+
+export default FeaturedProducts;

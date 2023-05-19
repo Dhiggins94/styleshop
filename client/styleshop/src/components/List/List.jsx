b8f041343394a3ea3aa6c981a0a806289a0fe43e
Divine Higgins <dhiggins94@gmail.com>
2023-05-19 15:07:24 -0400

    can now fetch data for single product, now starting shopping cart using react redux

--- client/styleshop/src/components/List/List.jsx
+++ client/styleshop/src/components/List/List.jsx
@@ -1,15 +1,16 @@
+
 import React from "react";
 import "./List.scss";
 import Card from "../Card/Card";
 import useFetch from "../../hooks/useFetch";
+
 const List = ({ subCats, maxPrice, sort, catId }) => {
   const { data, loading, error } = useFetch(
     `/products?populate=*&[filters][categories][id]=${catId}${subCats.map(
       (item) => `&[filters][sub_categories][id][$eq]=${item}`
     )}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`
   );
-// above is the strapi products filtering method used from the documentation(everything up till the {catId}). since we cant send an array directyly using strapi(strapi would have you send each item one by one using their filtering method) we will use the map function to create a new filter for each item(the subcat mapping will look for an item and then each item gets its own filtered subcategories). next will be the filtering of price using the lowest price(lowest price we took from the documentation) and the maxprice(our max price we setted). since the application could get bigger and slower depending on how many products to filter, it may be worth to implement button inputs for price filtering to refetch data.  next we're gonna use the documentations sort query for sorting prices and it can be set in asecding or descending order(with the help of our sort prop). strapi also uses its sort functioinality using the qs library which helps for cleaner and more complex queries which doesnt need to be in a string like this and uses objects
-  
+
   return (
     <div className="list">
       {loading
@@ -20,3 +21,4 @@ const List = ({ subCats, maxPrice, sort, catId }) => {
 };
 
 export default List;
+// above is the strapi products filtering method used from the documentation(everything up till the {catId}). since we cant send an array directyly using strapi(strapi would have you send each item one by one using their filtering method) we will use the map function to create a new filter for each item(the subcat mapping will look for an item and then each item gets its own filtered subcategories). next will be the filtering of price using the lowest price(lowest price we took from the documentation) and the maxprice(our max price we setted). since the application could get bigger and slower depending on how many products to filter, it may be worth to implement button inputs for price filtering to refetch data.  next we're gonna use the documentations sort query for sorting prices and it can be set in asecding or descending order(with the help of our sort prop). strapi also uses its sort functioinality using the qs library which helps for cleaner and more complex queries which doesnt need to be in a string like this and uses objects
--- client/styleshop/src/pages/Product/Product.jsx
+++ client/styleshop/src/pages/Product/Product.jsx
@@ -3,71 +3,94 @@ import "./Product.scss";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import BalanceIcon from "@mui/icons-material/Balance";
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 
 const Product = () => {
-  const [selectedImg, setSelectedImg] = useState(0);
+  const id = useParams().id;
+  const [selectedImg, setSelectedImg] = useState("image");
   const [quantity, setQuantity] = useState(1);
 
-  const images = [
-    "https://images.pexels.com/photos/10026491/pexels-photo-10026491.png?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    "https://images.pexels.com/photos/12179283/pexels-photo-12179283.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-  ];
+  const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
 
   return (
     <div className="product">
-      <div className="left">
-        <div className="images">
-          <img src={images[0]} alt="" onClick={(e) => setSelectedImg(0)} />
-          <img src={images[1]} alt="" onClick={(e) => setSelectedImg(1)} />
-        </div>
-        <div className="mainImg">
-          <img src={images[selectedImg]} alt="" />
-        </div>
-      </div>
-      <div className="right">
-        <h1>Title</h1>
-        <span className="price">$199</span>
-        <p>
-          {" "}
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet
-          consectetur adipiscing elit duis tristique sollicitudin nibh sit.{" "}
-        </p>
-        <div className="quantity">
-          {/* this exisit so anytime we press the minus button beyond one it stays at 1 via the teneary operator. so it takes into account if the previous value was 1 to keep it and if not make the prev value - 1 if its higher so it doesnt dip below zero */}
-          <button
-            onClick={() => setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}
-          >
-            -
-          </button>
-          {quantity}
-          <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
-        </div>
-        <button className="add">
-          <AddShoppingCartIcon />
-        </button>
-        <div className="links">
-          <div className="item">
-            <FavoriteBorderIcon/> ADD TO WISH LIST
+      {loading ? (
+        "loading"
+      ) : (
+        <>
+          <div className="left">
+            <div className="images">
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes?.img?.data?.attributes?.url
+                }
+                alt=""
+                onClick={(e) => setSelectedImg("img")}
+              />
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes?.img2?.data?.attributes?.url
+                }
+                alt=""
+                onClick={(e) => setSelectedImg("img2")}
+              />
+            </div>
+            <div className="mainImg">
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes[selectedImg]?.data?.attributes?.url
+                }
+                alt=""
+              />
+            </div>
           </div>
-          <div className="item">
-            <BalanceIcon/> ADD TO COMPARE
+          <div className="right">
+            <h1>{data?.attributes?.title}</h1>
+            <span className="price">${data?.attributes?.price}</span>
+            <p>{data?.attributes?.desc}</p>
+            <div className="quantity">
+              {/* this exisit so anytime we press the minus button beyond one it stays at 1 via the teneary operator. so it takes into account if the previous value was 1 to keep it and if not make the prev value - 1 if its higher so it doesnt dip below zero */}
+              <button
+                onClick={() =>
+                  setQuantity((prev) => (prev === 1 ? 1 : prev - 1))
+                }
+              >
+                -
+              </button>
+              {quantity}
+              <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+            </div>
+            <button className="add">
+              <AddShoppingCartIcon /> ADD TO CART
+            </button>
+            <div className="links">
+              <div className="item">
+                <FavoriteBorderIcon /> ADD TO WISH LIST
+              </div>
+              <div className="item">
+                <BalanceIcon /> ADD TO COMPARE
+              </div>
+            </div>
+            <div className="info">
+              <span>Vendor: Polo</span>
+              <span>Product Type: T-Shirt</span>
+              <span>Tag: T-Shirt, Women, Top</span>
+            </div>
+            <hr />
+            <div className="info">
+              <span>DESCRIPTION</span>
+              <hr />
+              <span>ADDITIONAL INFORMATION</span>
+              <hr />
+              <span>FAQ</span>
+            </div>
           </div>
-        </div>
-        <div className="info">
-          <span>Vender: Polo</span>
-          <span>Product Type: T-shirt</span>
-          <span>Tag: T-shirt, women, Top</span>
-        </div>
-        <hr />
-        <div className="info">
-          <span>DESCRIPTION</span>
-          <hr />
-          <span>ADDITIONAL INFORMATIOIN</span>
-          <hr />
-          <span>FAQ</span>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 };

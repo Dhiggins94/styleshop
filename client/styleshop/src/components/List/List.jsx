5485418a25c124fcc40a282d8cc191a111e638d7
Divine Higgins <dhiggins94@gmail.com>
2023-05-19 14:21:46 -0400

    finished filtering, now starting single product data fetching

--- client/styleshop/src/components/List/List.jsx
+++ client/styleshop/src/components/List/List.jsx
@@ -1,50 +1,20 @@
 import React from "react";
 import "./List.scss";
-import Card from "../Card/Card"
-const List = () => {
-  const data = [
-    {
-      id: 1,
-      img: "https://images.pexels.com/photos/8498312/pexels-photo-8498312.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      img2: "https://images.pexels.com/photos/8498314/pexels-photo-8498314.jpeg",
-      title: "Suit",
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 2,
-      img: "https://images.pexels.com/photos/14917974/pexels-photo-14917974.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      img2: "https://images.pexels.com/photos/14917972/pexels-photo-14917972.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      title: "Hat",
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 3,
-      img: "https://images.pexels.com/photos/8499385/pexels-photo-8499385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      title: "Dress",
-      img2: "https://images.pexels.com/photos/8499397/pexels-photo-8499397.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 4,
-      img: "https://images.pexels.com/photos/8498401/pexels-photo-8498401.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      img2: "https://images.pexels.com/photos/8498411/pexels-photo-8498411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      title: "Shirt",
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-  ];
+import Card from "../Card/Card";
+import useFetch from "../../hooks/useFetch";
+const List = ({ subCats, maxPrice, sort, catId }) => {
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&[filters][categories][id]=${catId}${subCats.map(
+      (item) => `&[filters][sub_categories][id][$eq]=${item}`
+    )}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`
+  );
+// above is the strapi products filtering method used from the documentation(everything up till the {catId}). since we cant send an array directyly using strapi(strapi would have you send each item one by one using their filtering method) we will use the map function to create a new filter for each item(the subcat mapping will look for an item and then each item gets its own filtered subcategories). next will be the filtering of price using the lowest price(lowest price we took from the documentation) and the maxprice(our max price we setted). since the application could get bigger and slower depending on how many products to filter, it may be worth to implement button inputs for price filtering to refetch data.  next we're gonna use the documentations sort query for sorting prices and it can be set in asecding or descending order(with the help of our sort prop). strapi also uses its sort functioinality using the qs library which helps for cleaner and more complex queries which doesnt need to be in a string like this and uses objects
+  
   return (
     <div className="list">
-      {/* {data?.map((item) => (
-        <Card item={item} key={item.id} />
-      ))} */}
+      {loading
+        ? "loading"
+        : data?.map((item) => <Card item={item} key={item.id} />)}
     </div>
   );
 };
--- client/styleshop/src/pages/Products/Products.jsx
+++ client/styleshop/src/pages/Products/Products.jsx
@@ -2,29 +2,45 @@ import React, { useState } from "react";
 import "./Products.scss";
 import List from "../../components/List/List";
 import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 const Products = () => {
   //  to fetch data using our category ID, using react router dom's useparams, we use parseInt since our id is in object as a String instead of a number.
   const catId = parseInt(useParams().id);
   const [maxPrice, setMaxPrice] = useState(1000);
   const [sort, setSort] = useState(null);
+  const [selectedSubCats, setselectedSubCats] = useState([]);
+
+  const { data, loading, error } = useFetch(
+    `/sub-categories?[filters][categories][id][$eq]=${catId}`
+  );
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const isChecked = e.target.checked;
+
+    setselectedSubCats(
+      isChecked
+        ? [...selectedSubCats, value]
+        : selectedSubCats.filter((item) => item !== value)
+    );
+  };
 
   return (
     <div className="products">
       <div className="left">
         <div className="filterItem">
           <h2>Product Categories</h2>
-          <div className="inputItem">
-            <input type="checkbox" value={1} id="1" />
-            <label htmlFor="1">Shoes</label>
-          </div>
-          <div className="inputItem">
-            <input type="checkbox" value={2} id="2" />
-            <label htmlFor="2">Shirts</label>
-          </div>
-          <div className="inputItem">
-            <input type="checkbox" value={3} id="3" />
-            <label htmlFor="3">Hats</label>
-          </div>
+          {data?.map((item) => (
+            <div className="inputItem" key={item.id}>
+              <input
+                type="checkbox"
+                value={item.id}
+                id={item.id}
+                onChange={handleChange}
+              />
+              <label htmlFor="1">{item.attributes.title}</label>
+            </div>
+          ))}
         </div>
         {/* this handles the filtering */}
         <div className="filterItem">
@@ -71,7 +87,7 @@ const Products = () => {
           alt=""
         />
         {/* this is the list of our clothes data from card.jsx mapped */}
-        <List catId={catId} maxPrice={maxPrice} sort={sort} />
+        <List catId={catId} maxPrice={maxPrice} sort={sort} subCats={selectedSubCats } />
       </div>
     </div>
   );
